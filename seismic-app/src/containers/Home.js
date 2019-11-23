5946fa15ab80067c2d8de7a87b823b4a45c263c9
Marius Cojocariu <marius@nyxpoint.com>
2019-11-23 13:27:27 +0200

    added router, and a basic structure that is open for debate

---
+++ seismic-app/src/containers/Home.js
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "../logo.svg";
+import LinkButton from "../components/LinkButton";
+import '../App.css';
+
+const Home = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <LinkButton to="/blog" type="primary">Blog</LinkButton>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+);
+
+export default Home;
