25c265ef5e153d05ab33e6d92484c2df657d49ac
Gabriel Tunaru <gabi.tunaru24@gmail.com>
2019-12-14 13:38:23 +0200

    created footer
    footer cleanup
    
    footer css text color

--- seismic-app/src/App.js
+++ seismic-app/src/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./containers/Home";
 import Blog from "./containers/Blog";
+import Footer from "./components/Footer";
+import Terms from "./containers/Terms";
+import Policy from "./containers/Policy";
 
 function App() {
   return (
@@ -17,7 +16,14 @@ function App() {
         <Route path="/blog">
           <Blog />
         </Route>
+        <Route path="/termeni-si-conditii">
+          <Terms />
+        </Route>
+        <Route path="/politica-de-confidentialitate">
+          <Policy />
+        </Route>
       </Switch>
+      <Footer />
     </Router>
   );
 }
