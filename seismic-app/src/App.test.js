53901588727a4658cb4af38f7154f6ca7c68e625
Daniel <dansto13@yahoo.com>
2019-12-17 22:00:01 +0100

    Setup unit tests framework.

--- seismic-app/src/App.test.js
+++ seismic-app/src/App.test.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+afterEach(cleanup);
+
+describe("App component", () => {
+  it("should render correctly", () => {
+    const container = render(<App />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
 });
