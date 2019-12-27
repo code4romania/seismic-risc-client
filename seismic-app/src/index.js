c5425c5284daef391ccc75468e183d24a9bf6e0c
Marius Cojocariu <marius@nyxpoint.com>
2019-12-27 20:14:17 +0200

    Marius cojocariu blog (#20)
    * added router, and a basic structure that is open for debate
    
    * basic layout for blog page, excluding header, footer and social links which should be added separately
    
    Co-authored-by:  Marius Cojocariu <marius@nyxpoint.com>

--- seismic-app/src/index.js
+++ seismic-app/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'typeface-open-sans';
+import 'typeface-titillium-web';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
